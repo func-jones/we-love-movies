325d35045ef06e8a8d1fa94ea439935266be1dd4
austinhjones3 <austin@austinjones.io>
2021-04-12 11:14:55 -0500

    knex table schemas written

---
+++ src/db/migrations/20210412104157_movies_theaters.js
@@ -0,0 +1,19 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("movies_theaters", (table) => {
+    table
+      .foreign("movie_id")
+      .references("movie_id")
+      .inTable("movies")
+      .onDelete("CASCADE");
+    table
+      .foreign("theater_id")
+      .references("theater_id")
+      .inTable("theaters")
+      .onDelete("CASCADE");
+    table.boolean("is_showing");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("movies_theaters");
+};
