7d60b9e47fbec7fd490342039724804198f3c02c
austinhjones3 <austin@austinjones.io>
2021-04-13 20:31:36 -0500

    added cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const theatersRouter = require("./theaters/theaters.router");
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const notFound = require("./errors/notFound");
 const errorHandler = require("./errors/errorHandler");
 app.use(express.json());
+app.use(cors());
 
 app.use("/theaters", theatersRouter);
 app.use("/movies", moviesRouter);
